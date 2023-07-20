ccf807a51a4bb14317c20affa938dc2872d0d0cf
Aurin Bose <48689587+aurinbose@users.noreply.github.com>
2023-07-20 06:26:25 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,24 +1,86 @@
 // Create Web Server
-// Create Express Application
-// Create Router
-// Create Route
-// Add Route to Router
-// Add Router to Application
-// Listen for Request
-const express = require('express');
-const app = express();
-const router = express.Router();
-const port = 3000;
+// 1. Create a web server object
+// 2. Create a web server
+// 3. Run the web server
 
-// Add Route to Router
-router.get('/', (req, res) => {
-    res.send('Comments Index Page');
+// Import the express module
+const express = require('express');
+
+// Create a web server object
+const app = express();
+
+// Create a web server
+app.listen(3000, () => {
+    console.log('Web server running on port 3000');
 });
 
-// Add Router to Application
-app.use('/comments', router);
+// Import the data module
+const data = require('./data');
 
-// Listen for Request
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+// Import the body-parser module
+const bodyParser = require('body-parser');
+
+// Use the body-parser module
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Use the body-parser module
+app.use(bodyParser.json());
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Set the route for the home page
+app.get('/', (req, res) => {
+    res.render('pages/index', {
+        title: 'Home Page',
+        comments: data.comments
+    });
+});
+
+// Set the route for the about page
+app.get('/about', (req, res) => {
+    res.render('pages/about', {
+        title: 'About Page'
+    });
+});
+
+// Set the route for the contact page
+app.get('/contact', (req, res) => {
+    res.render('pages/contact', {
+        title: 'Contact Page'
+    });
+});
+
+// Set the route for the comment page
+app.get('/comment', (req, res) => {
+    res.render('pages/comment', {
+        title: 'Comment Page'
+    });
+});
+
+// Set the route for the comment page
+app.post('/comment', (req, res) => {
+    console.log(req.body);
+    //res.send('You have submitted the comment');
+    data.comments.push(req.body);
+    res.redirect('/');
+});
+
+// Set the route for the comment page
+app.get('/delete/:id', (req, res) => {
+    console.log(req.params.id);
+    //res.send('You have submitted the comment');
+    data.comments.splice(req.params.id, 1);
+    res.redirect('/');
+});
+
+// Set the route for the comment page
+app.get('/edit/:id', (req, res) => {
+    console.log(req.params.id);
+    //res.send('You have submitted the comment');
+    res.render('pages/comment', {
+        title: 'Edit Comment',
+        comment: data.comments[req.params.id],
+        id: req.params.id
+    });
 });
